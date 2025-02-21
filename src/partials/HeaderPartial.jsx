c2be67a6d8114a8d0ab2a01148a218ee8642210c
Micael Fone <micaelfone0@gmail.com>
2025-02-20 23:20:02 -0300

    refactor: :wheelchair: Refatorando a header para ser melhor entendido

--- src/partials/HeaderPartial.jsx
+++ src/partials/HeaderPartial.jsx
@@ -10,10 +10,9 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Cadastrar</Nav.Link>
-            <Nav.Link href="/perguntascadastradas">Questionarios criados</Nav.Link>
-            <Nav.Link href="/perguntasdisponiveis">Responder questionários</Nav.Link>
-            <Nav.Link href="/contagemvotos">Contagem votos</Nav.Link>
+            <Nav.Link href="/">Cadastrar pergunta</Nav.Link>
+            <Nav.Link href="/perguntascadastradas">Questionários criados</Nav.Link>
+            <Nav.Link href="/perguntasdisponiveis">Questionários disponível para responder</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
